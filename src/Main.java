37278ab37c0235db358dce4f707d9966171bdc59
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-11-25 14:39:14 -0500

    boi

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please put in your name");
+		String name = in.next();
+		System.out.println("Hi "+name+"!");
+		System.out.println("Please put in a number for an array");
+		int size=in.nextInt();
+		double[] nums=new double[size];
+		for (int i = 0; i < nums.length; i++) {
+			
+		}
+
+
+	}
+
+}
