bb9e990a6c127028973f38f77c9bce82e6bb0d50
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-11-29 14:40:25 -0500

    Create Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Variables {
+//	public static void 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in= new Scanner(System.in);
+		System.out.println("please put in an integer:");
+		int boi=in.nextInt();
+		System.out.println("please put in a double:");
+		double bruv= in.nextDouble();
+		System.out.println("please put in a string:");
+		
+
+		
+	}
+
+}
