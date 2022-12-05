c2f20992c49c149f70a86930325ef1838d674c63
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-12-05 14:01:30 -0500

    Update Nested.java

--- src/Nested.java
+++ src/Nested.java
@@ -1,3 +1,35 @@
+//import java.util.Scanner;
+//public class Nested {
+////	public static void 
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		Scanner in= new Scanner(System.in);
+//		System.out.println("What's the first value?");
+//		int boi= in.nextInt();
+//		System.out.println("What's the second value?");
+//		int moustache= in.nextInt();
+//		if(moustache>boi) {
+//		for(int i=1; i<=moustache; i++) {
+//			for (int j=1; j<=boi; j++) {
+//				System.out.print(j*i+"\t");
+//			}
+//			System.out.println("\n");
+//			} 
+//		}
+//			if (moustache<boi) {
+//				for(int i=1; i<=boi; i++) {
+//					for (int j=1; j<boi; j++) {
+//						System.out.print(j*i+"\t");
+//					}
+//					System.out.println("\n");
+//			
+//			}
+//			
+//		}
+//		in.close();
+//	}
+//
+//}
 import java.util.Scanner;
 public class Nested {
 //	public static void 
@@ -8,12 +40,31 @@ public class Nested {
 		int boi= in.nextInt();
 		System.out.println("What's the second value?");
 		int moustache= in.nextInt();
+		if (moustache > boi) {
+		for(int i=1; i<=boi; i++) {
+			for (int j=1; j<=moustache; j++) {
+				System.out.print(j*i+"\t");
+			}
+			System.out.println("\n");
+		}
+	}
+		if (moustache < boi ) {
 		for(int i=1; i<=moustache; i++) {
 			for (int j=1; j<=boi; j++) {
 				System.out.print(j*i+"\t");
 			}
 			System.out.println("\n");
 		}
+	} 
+		if (moustache == boi ) {
+			for(int i=1; i<=moustache; i++) {
+				for (int j=1; j<=boi; j++) {
+					System.out.print(j*i+"\t");
+				}
+				System.out.println("\n");
+			}
+		} 
+	
+		
 	}
-
 }
