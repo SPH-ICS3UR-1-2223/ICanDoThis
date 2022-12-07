893a1db8783f1c91408f1715e5f069fa5fa61184
RubenLeszczynski <ruben.leszczynski@stu.ocsb.ca>
2022-12-07 11:32:48 -0500

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -9,8 +9,21 @@ public class Variables {
 		System.out.println("please put in a double:");
 		double bruv= in.nextDouble();
 		System.out.println("please put in a string:");
-		bruv= (Int)bruv;
-
+		String asdf= in.next();
+		int bruvi = (int)Math.round(bruv);
+		int temp;
+		temp = bruvi;
+		bruvi = boi;
+		boi = temp;
+		String cake = asdf.toUpperCase();
+		String doi= cake.substring(0, 3);
+		String goi= cake.substring(3, (cake.length()));
+		System.out.println("Your rounded double: \n"+ boi );
+		System.out.println("Your integer: \n"+ bruvi);
+		System.out.println("Your uppercased string: ");
+		System.out.println(doi);
+		System.out.println(goi);
+		
 		
 	}
 
